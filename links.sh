4fff7a009ccf334ba3833700bcf43584306126ad
Robnarok <robin-kraft@live.de>
2022-06-16 14:11:54 +0200

    links: comment Bash script

--- links.sh
+++ links.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# I3
 mkdir -p $HOME/.config/i3
 ln -f ./i3-config $HOME/.config/i3/config
 
+# Nvim
 mkdir -p $HOME/.config/nvim
 ln -f ./vim-config.vim $HOME/.config/nvim/init.vim
+touch $HOME/.config/nvim/extra.vim
 
+# zshrc
 ln -f ./zsh-config $HOME/.zshrc
